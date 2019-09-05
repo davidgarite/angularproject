dfa7365914eb2c295a903e3889c496146002cc1d
Anthony Gaona <gaonaanthony@gmail.com>
2019-09-04 19:31:54 -0700

    added three components. basic global styles. added general HTML layout in app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
+  // HTML
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    ProductsComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule

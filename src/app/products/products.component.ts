dddc8537eeca8a3bff2c51cb753c60b88e436103
Anthony Gaona <gaonaanthony@gmail.com>
2019-09-10 19:58:43 -0700

    added dummy data for products component. added ngFor template item to products component.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  Title: String = 'Products Area';
+  products = [
+    { title: 'John\'s Shirt', image: 'images/j-shirt.jpg', cost: 9.99 },
+    { title: 'John\'s Shirt', image: 'images/j-shirt.jpg', cost: 9.99 },
+    { title: 'John\'s Shirt', image: 'images/j-shirt.jpg', cost: 9.99 },
+    { title: 'John\'s Shirt', image: 'images/j-shirt.jpg', cost: 9.99 }
+  ]
 
   constructor() { }
 
